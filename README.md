eff43499a487363eacb39d860840928faf78325a
Lucas Becker <lucasbeckerfelisberto@gmail.com>
2020-06-05 18:57:52 -0300

    Update README

--- README.md
+++ README.md
@@ -1,11 +1,28 @@
 # <next_level_week/>
 > Projeto desevolvido durante a primeira Next Level Week da [Rocketseat](https://rocketseat.com.br/).
 
-NODE.js v12.17.0
-NPM v6.14.4
-![](https://img.shields.io/badge/node.js-12.17.0-blueviolet) ![](https://img.shields.io/badge/npm-6.14.4-green)
+![](https://img.shields.io/badge/node.js-12.17.0-6633CC) | ![](https://img.shields.io/badge/npm-6.14.4-34CB79) | ![](https://img.shields.io/badge/web-react-6633CC) | ![](https://img.shields.io/badge/mobile-react-native-34CB79)
+
+O projeto intitulado "Ecoleta" foi inicialmente desenvolvido durante a primeira Next Level Week da Rocketseat com o intuito de aprender na prática ferramentas e conceitos. 
 
 ![](https://github.com/lucasbecker/next-level-week/blob/master/header.png?raw=true)
+
+## Objetivo
+
+Objetivo do projeto desenvolvido.
+
+## Tecnologias
+
+### Server
+API RESTful com node.
+
+### Web
+React
+
+### Mobile
+React-Native
+
+## Proximos Passos
 
 ## API RESTful
 ### Back-end (Node.js)
--- server/src/server.ts
+++ server/src/server.ts
@@ -6,64 +6,12 @@ import { errors } from 'celebrate';
 
 const app = express();
 
-
 app.use(cors());
 app.use(express.json());
 
 app.use(routes);
 
 app.use('/uploads', express.static(path.resolve(__dirname, '..','uploads')));
-
-// Rota:    Endereço completo da requisição (localhost:3333/users)
-// Recurso: Qual entidade estamos acessando (/users)
-
-// Requisições
-// GET: Buscar uma ou mais informações do backend
-// POST: Criar uma nova informação no backend
-// PUT: Atualizar uma informação existente no backend
-// DELETE: Remover uma informação do backend
-
-// Parametros
-// Request Param: Parametros quem vem na propria rota que identificam um recurso
-// Query Param: Parametros que vem na propria rota geralmente opcionais para filtro, paginação e outros
-// Request Boby: Parametros para criação e atualização de informações
-
-// SQL
-//  SELECT * FROM users WHERE name = 'Diego'
-// KNEX.JS
-// knex('users').where('name','Diego').select('*');
-
-/*const users = [
-    "Diego", 
-    "Cleiton", 
-    "Robson", 
-    "Lucas"
-];
-
-app.get('/users', (request, response) => {
-    console.log('Listagem de Usuários');
-
-    const search = String(request.query.search);
-    
-    const filteredUsers = search ? users.filter(user => user.includes(search)) : search;
-
-    response.json(filteredUsers);
-});
-
-app.get('/users/:id', (request, response) => {
-    const id = Number(request.params.id);
-    const user = users[id];
-    return response.json(user);
-});
-
-app.post('/users', (request, response) => {
-    const data = request.body;
-    const user = {
-        name: data.name,
-        email: data.email
-    };
-    return response.json(user);
-});*/
 
 app.use(errors());
 
